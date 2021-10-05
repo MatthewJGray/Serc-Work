a8cb0fa0cf7b02c9e99c0be957010317dc081ed8
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 13:59:05 +0100

    Add files via upload

---
+++ EmployeeArrayExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EmployeeArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array
+            String[] strEName = new string[10];
+            String[] strJobTitle = new string[10];
+
+            int ArrayCount = 6;// count varriable to hold position of last element
+
+            for (int intLoopCounter = 0; intLoopCounter < ArrayCount; intLoopCounter++)
+            {
+                //ArrayCount = ArrayCount + 1;
+                Console.Write("");
+                Console.Write("Record " + intLoopCounter + "Enter Employee Name");
+                strEName[intLoopCounter] = Console.ReadLine();
+
+                Console.Write("Record " + intLoopCounter + "Enter  Job Title");
+                strJobTitle[intLoopCounter] = Console.ReadLine();
+            }
+            //display the information
+            for (int intLoopCounter = 0; intLoopCounter < ArrayCount; intLoopCounter++)
+            {
+                Console.Write("Name: " + strEName[intLoopCounter]);
+                Console.Write("Job Title: " + strJobTitle[intLoopCounter]);
+            }
+       
+        }
+    }
+}
