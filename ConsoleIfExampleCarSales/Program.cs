2cc250dfcdb34f06d774e7230b615b12e02ba848
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 13:54:21 +0100

    Add files via upload

---
+++ ConsoleIfExampleCarSales/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleIfExampleCarSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            double dblCommissionRate = 0, dblcommissionValue = 0;
+            int intValueofCarSales = 0;
+
+            //input
+            Console.Write("Enter the value of the car: ");
+            intValueofCarSales = Convert.ToInt32(Console.ReadLine());
+            //Process
+            if (intValueofCarSales < 10000)
+            {
+                dblCommissionRate = 1.5;
+            }
+            else if (intValueofCarSales <= 20000)
+            {
+                dblCommissionRate = 2.5;
+            }
+            else if (intValueofCarSales <= 40000)
+                dblCommissionRate = 5;
+            else
+            {
+                dblCommissionRate = 7.5;            
+            }
+            //calculate commission value
+            dblcommissionValue = dblCommissionRate / 100 * intValueofCarSales;
+
+            //output
+            Console.WriteLine("Commission Rate: " + dblCommissionRate);
+            Console.WriteLine("Commission Value: " + dblcommissionValue);
+
+        }
+    }
+}
