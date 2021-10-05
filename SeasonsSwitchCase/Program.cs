ffdc019a2d1e8cbb9505b7250ac71d3bb31c52ef
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 14:01:17 +0100

    Add files via upload

---
+++ SeasonsSwitchCase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SeasonsSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            string strMonth = "";
+
+            //input
+            Console.Write("Enter Month: ");
+            strMonth = Console.ReadLine();
+            //switch case
+            switch (strMonth)
+            {
+                //winter
+                case "December":
+                case "Janunary":
+                case "Febuary":
+                    Console.WriteLine("Winter");
+                    break;
+                //spring
+                case "March":
+                case "April":
+                case "May":
+                    Console.WriteLine("Spring");
+                    break;
+                //Summer
+                case "June":
+                case "July":
+                case "August":
+                    Console.WriteLine("Summer");
+                    break;
+                //Autumn
+                case "September":
+                case "October":
+                case "November":
+                    Console.WriteLine("Autumn");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
