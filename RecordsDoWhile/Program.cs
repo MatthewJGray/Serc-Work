9cba4fa7196b226df8d2adb2a1a9c78f8444bbc2
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 14:00:41 +0100

    Add files via upload

---
+++ RecordsDoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RecordsDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            String strArtistName = "", strAlbumName = "";
+            Double dblPrice = 0;
+            int intQuantity = 0;
+            int i = 1;
+            //Do while Loop
+            do
+            {
+                Console.WriteLine("Please enter the name of the recording artist.");
+                strArtistName = Console.ReadLine();
+
+                Console.WriteLine("Please enter the name of the album.");
+                strAlbumName = Console.ReadLine();
+
+                Console.WriteLine("Please enter the price.");
+                dblPrice = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the quantity.");
+                intQuantity = Convert.ToInt32(Console.ReadLine());
+                i++;// itteration counter
+            }
+            while (i <=4);
+            {
+                
+                    }
+        }
+    }
+}
