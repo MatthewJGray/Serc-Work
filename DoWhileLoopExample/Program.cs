2389d9abefa30cb453077f93f2fa26a963519122
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 13:58:00 +0100

    Add files via upload

---
+++ DoWhileLoopExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DoWhileLoopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(" i = " + 1);
+            }
+            while (i <= 20);
+        }
+    }
+}
