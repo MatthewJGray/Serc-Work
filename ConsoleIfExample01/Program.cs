961db3ad8fc1a61158c07e553c5c2e6a82939850
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 13:53:51 +0100

    Add files via upload

---
+++ ConsoleIfExample01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleIfExample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+
+
+            int intNumber1 = 0;
+            const int intSpecifiedValue = 12;
+            String strOutput = "";
+
+            //input
+            Console.Write("Enter Input (Number) : ");
+            intNumber1 = Convert.ToInt32(Console.ReadLine());
+
+            //process
+            if (intNumber1 > intSpecifiedValue)
+            {
+                strOutput = "Number is greater than specified number";
+            }
+            //output
+            Console.WriteLine(strOutput);
+        }
+    }
+}
