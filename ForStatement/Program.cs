c7a2b7ca2912fccdd24c97667cd242839a8c89bd
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 13:59:33 +0100

    Add files via upload

---
+++ ForStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            String strArtist = "", strAlbum = "";
+            int intPrice = 0;
+            int intQuantity = 0;
+            int i = 1;
+
+            //Process
+            for (i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("Enter Artist Name");
+                strArtist = Console.ReadLine();
+
+                Console.WriteLine("Enter Album Name");
+                strAlbum = Console.ReadLine();
+
+                Console.WriteLine("Enter Price");
+                intPrice = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Quantity");
+                intQuantity = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
