0723dc26872f0add0d864d1c6f5c17f39a3e06ad
MatthewJGray <91874588+MatthewJGray@users.noreply.github.com>
2021-10-05 14:02:50 +0100

    Add files via upload

---
+++ WhileLoopExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileLoopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            int myInt = 0;
+            int Limit = 45;
+            while (myInt <= Limit)//condition
+            {
+                //code 2 execute
+                Console.WriteLine(" " + myInt);
+                myInt++;
+            }
+            Console.WriteLine("Limit reached");
+        }
+    }
+}
